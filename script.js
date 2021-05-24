1e9ea0b07a7342c5a54fff6dfbad1c345d2544d4
Viniciusmgm <vinicius.mirandagm@gmail.com>
2021-05-23 22:25:59 -0300

    criação comida

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right"
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+}
 
 function criarBG(){
     context.fillStyle = 'lightgreen'
@@ -18,6 +22,11 @@ function criarCobrinha(){
         context.fillStyle = 'green'
         context.fillRect(snake[cont].x, snake[cont].y, box, box)
     }
+}
+
+function drawFood(){
+    context.fillStyle = 'red'
+    context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener('keydown', update)
@@ -36,16 +45,17 @@ function update(event){
 function iniciarJogo(){
     if(snake[0].x > 15 * box && direction == "right"){
         snake[0].x = 0
-    }else if(snake[0].x < 0 * box && direction == "left"){
-        snake[0].x = 0
+    }else if(snake[0].x < 0 && direction == "left"){
+        snake[0].x = 16 * box
     }else if(snake[0].y > 15 * box && direction == "up"){
         snake[0].y = 0
-    }else if(snake[0].y < 0 * box && direction == "down"){
-        snake[0].y = 0
+    }else if(snake[0].y < 0 && direction == "down"){
+        snake[0].y = 16 * box
     }
 
     criarBG()
     criarCobrinha()
+    drawFood()
 
     let snakeX = snake[0].x
     let snakeY = snake[0].y
